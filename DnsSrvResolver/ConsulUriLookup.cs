c526c62d886bae1b95127717e96bfe712db45e5d
concdkl <brad.tillman_contractor@jmfamily.com>
2017-12-19 17:41:22 -0500

    initial checkin

---
+++ DnsSrvResolver/ConsulUriLookup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using DnsClient;
+
+namespace DnsSrvResolver
+{
+    public class ConsulUriLookup
+    {
+
+        private Stack<ServiceHostEntry> _entries;
+        public ConsulUriLookup(string dnsName)
+        {
+            var dns = new LookupClient(IPAddress.Loopback, 8500);
+
+            var ary = dns.ResolveService("service.consul", dnsName);
+
+            _entries = new Stack<ServiceHostEntry>(ary);
+        }
+
+
+        public Uri GetNextUri()
+        {
+           throw new NotImplementedException();
+        }
+    }
+}
